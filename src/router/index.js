188e4471bcec607ed59528e2746763f9ac70bb0a
erhilds <erickh.andreas@gmail.com>
2020-05-21 10:37:35 +0700

    feat(not-found): add not-found page when user hit page that doesn't exist

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "@/views/Home.vue";
 import ThreadShow from "@/views/ThreadShow";
+import NotFound from "@/views/NotFound";
 
 Vue.use(VueRouter);
 
@@ -15,10 +16,12 @@ const routes = [
     path: "/thread/:id",
     name: "ThreadShow",
     props: true,
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: ThreadShow
+  },
+  {
+    path: "*",
+    name: "NotFound",
+    component: NotFound
   }
 ];
 
