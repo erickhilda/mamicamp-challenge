d27901ae9a22a98d3ed42e3eb566623653f4ccdf
erhilds <erickh.andreas@gmail.com>
2020-05-21 11:54:59 +0700

    feat(post): add feature to create new post

---
+++ src/components/Post/PostEditor.vue
@@ -0,0 +1,46 @@
+<template>
+  <form @submit.prevent="save">
+    <div class="form-group">
+      <textarea
+        name=""
+        id=""
+        cols="30"
+        rows="10"
+        class="form-input"
+        v-model="newPost"
+      />
+    </div>
+    <div class="form-actions">
+      <button class="btn-blue">Submit post</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  props: {
+    threadId: {
+      required: true
+    }
+  },
+  data() {
+    return {
+      newPost: ""
+    };
+  },
+  methods: {
+    save() {
+      const postId = "greatPost" + Math.random();
+      const post = {
+        text: this.text,
+        publishedAt: Math.floor(Date.now() / 1000),
+        threadId: this.threadId,
+        userId: "jUjmgCurRRdzayqbRMO7aTG9X1G2",
+        ".key": postId
+      };
+      this.newPost = "";
+      this.$emit("save", { post });
+    }
+  }
+};
+</script>
--- src/views/ThreadShow.vue
+++ src/views/ThreadShow.vue
@@ -2,42 +2,18 @@
   <div>
     <h1>{{ thread.title }}</h1>
 
-    <div class="post-list">
-      <div v-for="(postId, index) in thread.posts" class="post" :key="index">
-        <div class="user-info">
-          <a href="#" class="user-name">{{
-            users[posts[postId].userId].name
-          }}</a>
-
-          <a href="#">
-            <img
-              class="avatar-large"
-              :src="users[posts[postId].userId].avatar"
-              alt=""
-            />
-          </a>
-
-          <p class="desktop-only text-small">107 posts</p>
-        </div>
-
-        <div class="post-content">
-          <div>
-            {{ posts[postId].text }}
-          </div>
-        </div>
-
-        <div class="post-date text-faded">
-          {{ posts[postId].publishedAt }}
-        </div>
-      </div>
-    </div>
+    <post-list :posts="posts" />
+    <post-editor @save="addPost" :thread-id="id" />
   </div>
 </template>
 
 <script>
 import sourceData from "@/data";
+import PostList from "@/components/Post/PostList";
+import PostEditor from "@/components/Post/PostEditor";
 
 export default {
+  components: { PostList, PostEditor },
   props: {
     id: {
       required: true,
@@ -47,9 +23,26 @@ export default {
   data() {
     return {
       thread: sourceData.threads[this.id],
-      posts: sourceData.posts,
-      users: sourceData.users
+      newPost: ""
     };
+  },
+  computed: {
+    posts() {
+      const postIds = Object.values(this.thread.posts);
+      return Object.values(sourceData.posts).filter(post =>
+        postIds.includes(post[".key"])
+      );
+    }
+  },
+  methods: {
+    addPost(eventData) {
+      const post = eventData.post;
+      const postId = eventData.post[".key"];
+      this.$set(sourceData.posts, postId, post);
+      this.$set(this.thread.posts, postId, postId);
+      this.$set(sourceData.users[post.userId].posts, postId, postId);
+      this.newPost = "";
+    }
   }
 };
 </script>
