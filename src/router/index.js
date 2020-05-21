900aced2b075e064163b13c187d6386c91e700f3
erhilds <erickh.andreas@gmail.com>
2020-05-21 10:11:19 +0700

    feat: organize file under views directory and adding link to redirect to thread page

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "@/views/Home.vue";
-import ThreadShow from "@/components/Thread/ThreadShow";
+import ThreadShow from "@/views/ThreadShow";
 
 Vue.use(VueRouter);
 
