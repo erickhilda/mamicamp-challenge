bc0479279c40db27b85015efd39a404d4ba34035
erhilds <erickh.andreas@gmail.com>
2020-05-27 14:36:33 +0700

    chore: delete dotenv

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+"use strict";
 import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
@@ -6,22 +7,21 @@ import store from "./store";
 import AppDate from "@/components/AppDate";
 import firebase from "firebase";
 
-require("dotenv").config();
-
 Vue.component("AppDate", AppDate);
 
 Vue.config.productionTip = false;
-
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
+  measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
 };
+
+console.log(firebaseConfig);
 
 firebase.initializeApp(firebaseConfig);
 
