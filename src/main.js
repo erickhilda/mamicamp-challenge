2b2be996369bf2a7c42f172271185ec3a9f1604b
erhilds <erickh.andreas@gmail.com>
2020-05-27 13:55:51 +0700

    chore: install firebase and set up .env variables

--- src/main.js
+++ src/main.js
@@ -4,10 +4,26 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import AppDate from "@/components/AppDate";
+import firebase from "firebase";
+
+require("dotenv").config();
 
 Vue.component("AppDate", AppDate);
 
 Vue.config.productionTip = false;
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
