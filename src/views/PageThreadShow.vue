1a12392e5c44f64bc3f2e7d7081ac3062996fb68
erhilds <erickh.andreas@gmail.com>
2020-05-30 01:15:06 +0700

    fix: got userId of undefined when fetching thread post

--- src/views/PageThreadShow.vue
+++ src/views/PageThreadShow.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="asyncDataStatus_ready" class="col-full push-top">
+  <div v-if="asyncDataStatus_ready" class="col-large push-top">
     <h1>
       {{ thread.title }}
 
@@ -11,25 +11,22 @@
         Edit Thread
       </router-link>
     </h1>
-
     <p>
-      By <a href="#" class="link-unstyled">{{ user.name }},</a>
-      <app-date :timestamp="thread.publishedAt" />.
+      By <a href="#" class="link-unstyled">{{ user.name }}</a
+      >, <AppDate :timestamp="thread.publishedAt" />.
       <span
         style="float:right; margin-top: 2px;"
         class="hide-mobile text-faded text-small"
+        >{{ repliesCount }} replies by
+        {{ contributorsCount }} contributors</span
       >
-        {{ repliesCount }} replies by {{ contributorsCount }} contributors
-      </span>
     </p>
-
     <post-list :posts="posts" />
-    <post-editor v-if="authUser" :thread-id="id" />
-
+    <post-editor v-if="authUser" :threadId="id" />
     <div v-else class="text-center" style="margin-bottom: 50px;">
-      <router-link :to="{ name: 'SignIn', query: { redirectTo: $route.path } }">
-        Sign in
-      </router-link>
+      <router-link :to="{ name: 'SignIn', query: { redirectTo: $route.path } }"
+        >Sign in</router-link
+      >
       or
       <router-link
         :to="{ name: 'Register', query: { redirectTo: $route.path } }"
@@ -46,44 +43,43 @@ import { mapActions, mapGetters } from "vuex";
 import PostList from "@/components/Post/PostList";
 import PostEditor from "@/components/Post/PostEditor";
 import asyncDataStatus from "@/mixins/asyncDataStatus";
+import { objectPropertiesCounter } from "@/helpers/objectPropertiesCounter";
 
 export default {
-  components: { PostList, PostEditor },
+  components: {
+    PostList,
+    PostEditor
+  },
+  mixins: [asyncDataStatus],
   props: {
     id: {
       required: true,
       type: String
     }
   },
-  mixins: [asyncDataStatus],
   computed: {
     ...mapGetters({
       authUser: "auth/authUser"
     }),
-    posts() {
-      const postIds = Object.values(this.thread.posts);
-      return Object.values(this.$store.state.posts.items).filter(post =>
-        postIds.includes(post[".key"])
-      );
-    },
     thread() {
       return this.$store.state.threads.items[this.id];
-    },
-    user() {
-      return this.$store.state.users.items[this.thread.userId];
     },
     repliesCount() {
       return this.$store.getters["threads/threadRepliesCount"](
         this.thread[".key"]
       );
     },
+    user() {
+      return this.$store.state.users.items[this.thread.userId];
+    },
     contributorsCount() {
-      const replies = Object.keys(this.thread.posts)
-        .filter(postId => postId !== this.thread.firstPostId)
-        .map(postId => this.$store.state.posts[postId]);
-      const userIds = replies.map(post => post.userId);
-      return userIds.filter((item, index) => index === userIds.indexOf(item))
-        .length;
+      return objectPropertiesCounter(this.thread.contributors);
+    },
+    posts() {
+      const postIds = Object.values(this.thread.posts);
+      return Object.values(this.$store.state.posts.items).filter(post =>
+        postIds.includes(post[".key"])
+      );
     }
   },
   methods: {
@@ -92,10 +88,11 @@ export default {
     ...mapActions("posts", ["fetchPosts"])
   },
   created() {
+    // fetch thread
     this.fetchThread({ id: this.id })
       .then(thread => {
+        // fetch user
         this.fetchUser({ id: thread.userId });
-        this.fetchPosts({ ids: Object.keys(thread.posts) });
         return this.fetchPosts({ ids: Object.keys(thread.posts) });
       })
       .then(posts => {
