e272f63357fcd1c2b49d6f15c39016566b89c0b3
erhilds <erickh.andreas@gmail.com>
2020-05-29 20:56:43 +0700

    feat: display authenticate user profile

--- src/store/getters.js
+++ src/store/getters.js
@@ -5,6 +5,14 @@ export default {
     return state.authId ? state.users[state.authId] : null;
   },
 
+  userPosts: state => id => {
+    const user = state.users[id];
+    if (user.posts) {
+      return Object.values(state.posts).filter(post => post.userId === id);
+    }
+    return [];
+  },
+
   userThreadsCount: state => id =>
     objectPropertiesCounter(state.users[id].threads),
   userPostsCount: state => id => objectPropertiesCounter(state.users[id].posts),
--- src/views/PageProfile.vue
+++ src/views/PageProfile.vue
@@ -1,31 +1,46 @@
 <template>
   <div class="flex-grid">
-    <!-- <profile-card v-if="!edit" :user="user" />
-    <profile-card-editor v-else :user="user" /> -->
-    <div class="col-7 push-top">
-      <h1>My Profile</h1>
-      <!-- <div class="profile-header">
-        <span class="text-lead"> {{ user.name }} recent activity </span>
-        <a href="#">See only started threads?</a>
-      </div> -->
-      <!-- <hr />
-      <post-list :posts="userPosts" /> -->
+    <template v-if="user">
+      <profile-card v-if="!edit" :user="user" />
+      <profile-card-editor v-else :user="user" />
+      <div class="col-7 push-top">
+        <div class="profile-header">
+          <span class="text-lead"> {{ user.name }} recent activity </span>
+          <a href="#">See only started threads?</a>
+        </div>
+        <hr />
+        <post-list :posts="userPosts" />
+      </div>
+    </template>
+    <div v-else class="text-center" style="margin-bottom: 50px;">
+      <router-link :to="{ name: 'Signin', query: { redirectTo: $route.path } }">
+        Sign in
+      </router-link>
+      or
+      <router-link
+        :to="{ name: 'Register', query: { redirectTo: $route.path } }"
+      >
+        Register
+      </router-link>
+      to post a reply.
     </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
-// import PostList from "@/components/Post/PostList";
-// import ProfileCard from "@/components/Profile/ProfileCard";
-// import ProfileCardEditor from "@/components/Profile/ProfileCardEditor";
+import PostList from "@/components/Post/PostList";
+import ProfileCard from "@/components/Profile/ProfileCard";
+import ProfileCardEditor from "@/components/Profile/ProfileCardEditor";
+import asyncDataStatus from "@/mixins/asyncDataStatus";
 
 export default {
   components: {
-    // PostList,
-    // ProfileCard,
-    // ProfileCardEditor
+    PostList,
+    ProfileCard,
+    ProfileCardEditor
   },
+  mixins: [asyncDataStatus],
   props: {
     edit: {
       type: Boolean,
@@ -37,16 +52,13 @@ export default {
       user: "authenticatedUser"
     }),
     userPosts() {
-      if (this.user.posts) {
-        return Object.values(this.$store.state.posts).filter(
-          post => post.userId === this.user[".key"]
-        );
-      }
-      return [];
+      return this.$store.getters.userPosts(this.user[".key"]);
     }
   },
   created() {
-    this.$emit("ready");
+    this.$store
+      .dispatch("fetchPosts", { ids: this.user.posts })
+      .then(() => this.asyncDataStatus_fetched());
   }
 };
 </script>
